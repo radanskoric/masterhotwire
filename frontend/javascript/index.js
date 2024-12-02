d410659279cd47fdbaa356e46a5fcb059124d8c8
Radan Skorić <radan.skoric@gmail.com>
2024-12-02 14:19:18 +0100

    Add Turbo

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,6 @@
 import "$styles/index.css"
 import "$styles/syntax-highlighting.css"
+import * as Turbo from "@hotwired/turbo"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
